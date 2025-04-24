fdb6847339a9c2deccc5ee6cb9b296e6b76aeddb
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-04-24 17:23:42 +0300

    Добавил альтернативное изображение для элементов АГ

--- resources/frontend/atoms/government/Member.jsx
+++ resources/frontend/atoms/government/Member.jsx
@@ -5,15 +5,13 @@ export default function GovernmentMember({isHead, avatar, name, position, onClic
   const hasAvatar = avatar && !avatar.endsWith('/storage/null');
 
   return (
-    <button className={ `government-team__container government-team--${isHead ? 'head' : 'member'}` } onClick={onClick}>
-      {hasAvatar &&
-        <div className="government-team__avatar">
-          <img src={avatar} alt={name}/>
-        </div>
-      }
+    <button className={`government-team__container government-team--${isHead ? 'head' : 'member'}`} onClick={onClick}>
+      <div className="government-team__avatar">
+        <img src={hasAvatar ? avatar : "img/bg_overlay.png"} alt={name} />
+      </div>
       <div className="government-team__info">
-        <h2 className="government-team__title">{ name }</h2>
-        <div className="government-team__position">{ position }</div>
+        <h2 className="government-team__title">{name}</h2>
+        <div className="government-team__position">{position}</div>
       </div>
     </button>
   )
