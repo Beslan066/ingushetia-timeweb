ec34bfbfb6571565e992e9238cf1bccbecc87542
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-05-03 12:44:17 +0300

    Уменьшил отступ у элементов поддержки СВО 2

--- resources/frontend/atoms/links/internal-trigger-link.jsx
+++ resources/frontend/atoms/links/internal-trigger-link.jsx
@@ -3,12 +3,12 @@ import './internal-trigger.css'
 
 export default function InternalTriggerLink({ title, description, onClick }) {
   return (
-    <div className="internal-trigger" style={{marginBottom: '20px'}}>
+    <div className="internal-trigger" style={{marginBottom: '10px'}} onClick={ onClick }>
       <div className="internal-trigger__info">
         <h4 className="internal-trigger__title">{ title }</h4>
         { !!description && <span className="internal-trigger__description">{ description }</span> }
       </div>
-      <button onClick={ onClick } className="internal-trigger__link"><ChevronRightIcon color="neutral-dark"/>
+      <button className="internal-trigger__link"><ChevronRightIcon color="neutral-dark"/>
       </button>
     </div>
   )
