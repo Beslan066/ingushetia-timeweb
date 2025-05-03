6c00cf8d5462c1366a07c3bedb075c132a246428
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-05-03 15:10:51 +0300

    Поправил роуты региона

--- resources/frontend/molecules/navigation/regions-navigation.jsx
+++ resources/frontend/molecules/navigation/regions-navigation.jsx
@@ -8,15 +8,15 @@ export default function RegionsNavigation() {
   return (
     <nav className="regions-nav">
       <ul>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/Culture' ? 'active' : ''}` } href="/region">О регионе</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/History' ? 'active' : ''}` } href="/history">История</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/Economics' ? 'active' : ''}` } href="/economic">Экономика</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/Municipality' ? 'active' : ''}` } href="/municipality">Муниципальные образования</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/SocialEconomics' ? 'active' : ''}` } href="/social-economic-development">Социально-экономическое развитие</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/NationalProjects' ? 'active' : ''}` } href="/nation-projects">Национальные проекты</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/PresidentImplementations' ? 'active' : ''}` } href="/implementations">Реализация стратегических инициатив Президента РФ</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/MilitarySupport' ? 'active' : ''}` } href="/military-support">Поддержка семей военнослужащих</Link></li>
-        <li><Link className={ `regions-nav__link ${ component === 'Culture/CitizenSupport' ? 'active' : ''}` } href="/economic-support">Поддержка экономики и граждан</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/Region' ? 'active' : ''}` } href="/region">О регионе</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/History' ? 'active' : ''}` } href="/history">История</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/Economics' ? 'active' : ''}` } href="/economic">Экономика</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/Municipality' ? 'active' : ''}` } href="/municipality">Муниципальные образования</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/SocialEconomics' ? 'active' : ''}` } href="/social-economic-development">Социально-экономическое развитие</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/NationalProjects' ? 'active' : ''}` } href="/nation-projects">Национальные проекты</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/PresidentImplementations' ? 'active' : ''}` } href="/implementations">Реализация стратегических инициатив Президента РФ</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/MilitarySupport' ? 'active' : ''}` } href="/military-support">Поддержка семей военнослужащих</Link></li>
+        <li><Link className={ `regions-nav__link ${ component === 'Region/CitizenSupport' ? 'active' : ''}` } href="/economic-support">Поддержка экономики и граждан</Link></li>
       </ul>
     </nav>
   )
